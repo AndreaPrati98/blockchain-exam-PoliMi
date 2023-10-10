1f80fa611885116301aebe65f98483f8a516fc42
Andrea Prati <a.prati@outlook.it>
2023-10-10 10:38:06 +0200

    update customerNFT contract

--- flutter/smart-contracts/contracts/CustomerNFT.sol
+++ flutter/smart-contracts/contracts/CustomerNFT.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0; 
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // NFT's standard
 import "@openzeppelin/contracts/access/Ownable.sol"; // to handle the ownership of the contract
 import "@openzeppelin/contracts/utils/Counters.sol"; // to auto-increment the tokenID
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // NFT's standard
 
-contract CustomerNFT is ERC721, Ownable {
+contract CustomerNFT is ERC721URIStorage, Ownable {
 
+    mapping (uint256 => uint256) private subscription_book;
+    // keeping track of the tokenID
+    mapping(address => uint256) private mintedWallets;
+    
     IERC721 public adminNFT; // Reference to your AdminNFT contract
 
     using Counters for Counters.Counter;
@@ -26,10 +30,36 @@ contract CustomerNFT is ERC721, Ownable {
     }
 
     /// The tokenId is auto-incremented
-    function safeMint(address to) public onlyAdminNFTOwner {
+    function safeMint(address to, uint monthsValidity) public onlyAdminNFTOwner {
         uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        
         _safeMint(to, tokenId);
+
+        mintedWallets[to] = tokenId;
+
+        // here, in order to transform the monthsValidity in seconds we should 
+        // do monthsValidity * 30 days.
+        subscription_book[tokenId] = block.timestamp + monthsValidity * 30 seconds;
+
+        _tokenIdCounter.increment();
+    }
+
+    function accessGym() public view {
+        require(balanceOf(msg.sender) > 0, "The caller has no any CustomerNFT token");
+        uint256 tokenId = mintedWallets[msg.sender];
+        require(subscription_book[tokenId] > block.timestamp, "The subscription is expired");
+    }
+
+    function getCurrentTime() public view returns(uint){
+        return block.timestamp;
+    }
+
+    function getExpirationByTokenId(uint256 tokenId) public view returns(uint256) {
+        return subscription_book[tokenId];
+    }
+
+    function getTokenIdByAddress(address customerAddress) public view returns(uint256){
+        return mintedWallets[customerAddress];
     }
 
 }
